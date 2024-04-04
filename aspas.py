88255e0e8a0944ee763c969314673605c2bf0e3e
Mateus Nicolas <78376333+mattnhb@users.noreply.github.com>
2024-04-04 18:00:28 -0300

    Update aspas.py

--- aspas.py
+++ aspas.py
@@ -1,19 +1,26 @@
 import re
 
+pattern_none = r'None'
+pattern0 = r'"'
 pattern1 = r"(\s'\s)"
 pattern2 = r"(,)'|'(,)"
 pattern3 = r"(?<=\{)'|'(?=:)|'(?=\})|(\s)'"
 
 data = [
-    "{'pedro': 'panda', 'endereco':'casa d ' agua'}",
-    "{'pedro': 'panda', 'endereco': 'casa d' agua'}",
-    "{'pedro': 'panda','endereco': 'casa d'''' agua'}",
+    '{"None": "panda", "endereco":"casa d " agua"}',
+    "{'pedro': 'None', 'endereco': 'casa d' agua'}",
+    "[{'pedro': 'panda','endereco': 'casa d'''' agua'}]",
+    "None"
+
 ]
 
 for text in data:
+    
+    result = re.sub(pattern0, r'\n', text)
     result = re.sub(pattern1, r" ", text)
     result = re.sub(pattern2, r"\",", result)
     result = re.sub(pattern3, r"\"", result)
+    result = re.sub(pattern_none, 'null', text)
     print(result)
 
 from pyspark.sql import SparkSession
@@ -31,8 +38,9 @@ df = spark.createDataFrame(
         Row(data=_)
         for _ in [
             "{'pedro': 'panda', 'endereco': 'casa d ' agua', 'numero': '432'}",
-            "{'pedro': 'panda', 'endereco': 'casa d' agua', 'numero': '432'}",
-            "{'pedro': 'panda', 'endereco': 'casa d'''' agua', 'numero': '432'}",
+            "{'pedro': 'panda', 'endereco': 'casa d' agua', 'numero': '432'}, {'pedro': '321', 'endereco': '213 d' agua', 'numero': '132'}",
+            "[{'pedro': 'panda', 'endereco': 'casa d'''' agua', 'numero': '432'}]",
+            "None"
         ]
     ]
 )
@@ -50,6 +58,22 @@ df = (
 
 
 df.show(truncate=False)
+
+df = df.withColumn("data", regexp_replace("data", r'\[', r""))
+df = df.withColumn("data", regexp_replace("data", r'\]', r""))
+# df = df.withColumn("data", F.concat(F.lit("["), F.col("data"), F.lit("]")))
+df.show(truncate=False)
+# df = df.withColumn("data", F.array(F.col("data")))
+df.show(truncate=False)
+df.printSchema()
+
+df = df.withColumn("data", F.split(F.col("data"), ", "))
+
+# Explode the array into separate rows
+df = df.select(F.explode(F.col("data")).alias("data"))
+# df = df.withColumn("new_data", F.explode("data"))
+df.show(truncate=False)
+df.printSchema()
 
 df = df.select("data", F.json_tuple("data", "pedro", "endereco", "numero")).toDF("data", "pedro", "endereco", "numero")
 df.show(truncate=False)
